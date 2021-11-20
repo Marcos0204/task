2854dff1743fdb907a97396ff5b7ab5f93673244
Marcos Bastidas <marcosbastidas@gmail.com>
2021-11-20 13:14:54 -0400

    update

---
+++ front/src/components/tasks/FormTask.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FormTask = () => {
+  return (
+    <div className='formulario'>
+      <form>
+          .contenedor
+      </form>
+    </div>
+  )
+}
+
+export default FormTask
