e499063fdd2bd6390b9b2e05aa1feb055926fcaa
Marcos Bastidas <marcosbastidas@gmail.com>
2021-11-20 22:10:11 -0400

    update-context-config

--- front/src/context/projects/ProjectState.js
+++ front/src/context/projects/ProjectState.js
@@ -1,10 +1,18 @@
 import React, { useReducer } from 'react'
 import ProjectContext from './ProjectContext'
 import ProjectReducers from './ProjectReducers'
-import { FORM_PROJECT } from '../../types'
+import { FORM_PROJECT, GET_PROJECTS } from '../../types'
+
+
 
 const ProjectState = ({children}) => {
+    const projects= [
+        {id:1, name:'tienda virtual'},
+        {id:2, name:'tienda de front end'},
+        {id:3, name:'tienda de back end'},
+    ]
     const initialState = {
+        projects: [],
         form: false
     }
     const [ state, dispatch ] =useReducer(ProjectReducers, initialState);
@@ -16,11 +24,20 @@ const ProjectState = ({children}) => {
             type: FORM_PROJECT
         })
     }
+    // obtener proyects
+    const getProjects = () =>{
+        dispatch({
+            type: GET_PROJECTS,
+            payload: projects
+        })
+    }
     return (
         <ProjectContext.Provider
             value={{
+                projects: state.projects,
                 form: state.form,
-                showForm: showForm
+                showForm: showForm,
+                getProjects: getProjects
             }}
         >
             {children}
